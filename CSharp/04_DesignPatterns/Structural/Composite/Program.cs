21d5ec997369cb1da64739e8709197afec3a61fd
Renato-55 <renatoaobarbosa@gmail.com>
2023-09-03 15:09:21 +0100

    Composite

--- CSharp/04_DesignPatterns/Structural/Composite/Program.cs
+++ CSharp/04_DesignPatterns/Structural/Composite/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace CompositePattern
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+
+            // This way the client code can support the simple leaf
+            // components...
+            Leaf leaf = new Leaf();
+            Console.WriteLine("Client: I get a simple component:");
+            client.ClientCode(leaf);
+
+            // ...as well as the complex composites.
+            Composite tree = new Composite();
+            Composite branch1 = new Composite();
+            branch1.Add(new Leaf());
+            branch1.Add(new Leaf());
+            Composite branch2 = new Composite();
+            branch2.Add(new Leaf());
+            tree.Add(branch1);
+            tree.Add(branch2);
+            Console.WriteLine("Client: Now I've got a composite tree:");
+            client.ClientCode(tree);
+
+            Console.Write("Client: I don't need to check the components classes even when managing the tree:\n");
+            client.ClientCode2(tree, leaf);
+        }
+    }
+}
