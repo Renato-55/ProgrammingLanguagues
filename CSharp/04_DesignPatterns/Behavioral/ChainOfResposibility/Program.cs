121e96308a40bd1145bbe7fa1069e45ceca5b101
Renato-55 <renatoaobarbosa@gmail.com>
2023-09-04 20:45:11 +0100

    Chain Of Command

--- CSharp/04_DesignPatterns/Behavioral/ChainOfResposibility/Program.cs
+++ CSharp/04_DesignPatterns/Behavioral/ChainOfResposibility/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace RefactoringGuru.DesignPatterns.ChainOfResponsibility.Conceptual
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The other part of the client code constructs the actual chain.
+            var monkey = new MonkeyHandler();
+            var squirrel = new SquirrelHandler();
+            var dog = new DogHandler();
+
+            monkey.SetNext(squirrel).SetNext(dog);
+
+            // The client should be able to send a request to any handler, not
+            // just the first one in the chain.
+            Console.WriteLine("Chain: Monkey > Squirrel > Dog\n");
+            Client.ClientCode(monkey);
+            Console.WriteLine();
+
+            Console.WriteLine("Subchain: Squirrel > Dog\n");
+            Client.ClientCode(squirrel);
+        }
+    }
+}
