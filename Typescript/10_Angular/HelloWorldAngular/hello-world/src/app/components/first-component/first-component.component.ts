779bad3b0f9b710f00df0847826e873a16ea6008
Renato-55 <renatoaobarbosa@gmail.com>
2023-09-19 23:11:27 +0100

    Data to Html Components - Angular

--- Typescript/10_Angular/HelloWorldAngular/hello-world/src/app/components/first-component/first-component.component.ts
+++ Typescript/10_Angular/HelloWorldAngular/hello-world/src/app/components/first-component/first-component.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-first-component',
   templateUrl: './first-component.component.html',
-  styleUrls: ['./first-component.component.scss']
+  styleUrls: ['./first-component.component.scss'],
 })
 export class FirstComponentComponent {
-
+  name: string = 'Renato';
 }
