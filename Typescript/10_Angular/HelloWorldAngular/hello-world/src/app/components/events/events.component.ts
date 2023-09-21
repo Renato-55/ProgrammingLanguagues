2340a943db4e602f8c42a401d451136db4d9fc27
Renato-55 <renatoaobarbosa@gmail.com>
2023-09-21 23:48:47 +0100

    Simple "event" bttn click

---
+++ Typescript/10_Angular/HelloWorldAngular/hello-world/src/app/components/events/events.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-events',
+  templateUrl: './events.component.html',
+  styleUrls: ['./events.component.scss'],
+})
+export class EventsComponent {
+  show: boolean = true;
+
+  showMessage(): void {
+    this.show = !this.show;
+  }
+}
