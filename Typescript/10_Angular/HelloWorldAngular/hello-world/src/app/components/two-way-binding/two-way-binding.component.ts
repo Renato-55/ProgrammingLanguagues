87184d6c8446f838689bf6cb66535318bf7277c8
Renato-55 <renatoaobarbosa@gmail.com>
2023-09-23 16:33:59 +0100

    Two way data binding

---
+++ Typescript/10_Angular/HelloWorldAngular/hello-world/src/app/components/two-way-binding/two-way-binding.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-two-way-binding',
+  templateUrl: './two-way-binding.component.html',
+  styleUrls: ['./two-way-binding.component.scss'],
+})
+export class TwoWayBindingComponent {
+  name: string = '';
+}
